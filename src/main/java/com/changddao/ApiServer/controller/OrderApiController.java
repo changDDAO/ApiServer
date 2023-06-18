2c2747db3ad53bbd82c213922016a47124d1c354
Changhogod <younch8342@naver.com>
2023-06-18 21:00:15 +0900

    create OrderApiController & test fetchjoin &cnt query

--- src/main/java/com/changddao/ApiServer/controller/OrderApiController.java
+++ src/main/java/com/changddao/ApiServer/controller/OrderApiController.java
@@ -8,22 +8,50 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
     private final OrderService orderService;
-    @GetMapping("api/v3/orders")
-    public List<OrderQueryDto> ordersWithMemberResponse() {
-        return null; //작업해야될 부분들
-    }
+   @GetMapping("/api/v2/orders")
+    public List<OrderFetchedDto> ordersV2() {
+       List<Order> orders = orderService.ordersWithMember();
+       List<OrderFetchedDto> collect = orders.stream().map(order -> new OrderFetchedDto(order))
+               .collect(Collectors.toList());
+       return collect;
+   }
     @Data
-    static class OrderQueryDto{
-        private String memberName;
-        private String productName;
-        private int count;
-        private int orderPrice;
-        private List<OrderItem> orderItems;
+    private class OrderFetchedDto{
+       String username;
+       String phoneNum;
+       LocalDateTime orderTime;
+       public List<OrderItemDto> orderItems;
+       //entity를 직접 노출하면 안된다
+        // entity를 수정하는 경우 api스펙자체가 깨지기 때문에 entity는 무조건
+        //dto로 변환하여 반환하자
+
+        public OrderFetchedDto(Order order) {
+            this.username =order.getMember().getUsername();
+            this.phoneNum = order.getMember().getInfo().getPhoneNumber();
+            this.orderTime = order.getCreatedDate();
+            this.orderItems = order.getOrderItems().stream()
+                    .map(orderItem->new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+        @Data
+        private class OrderItemDto {
+            String productName;
+            int price;
+            int count;
+
+            public OrderItemDto(OrderItem orderItem) {
+                this.productName =orderItem.getItem().getName();
+                this.price = orderItem.getItem().getPrice();
+                this.count = orderItem.getCount();
+            }
+        }
     }
 }
